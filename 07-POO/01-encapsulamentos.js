84b8734141ebaeef11fadc788c15339b97299769
ANNA PAULA HENRIQUES SOUZA DIAS <26171895@edu119.local>
2026-08-20 16:56:50 -0300

    atualizando arquivo

---
+++ 07-POO/01-encapsulamentos.js
@@ -0,0 +1,17 @@
+class pessoa{
+    #nome;
+
+    constructor(nome){
+        this.nome = nome
+    }
+
+    get nome(){
+        return this.#nome
+    }
+
+
+    set nome(valor){
+        if(valor.lenght >= 2) this.#nome = valor;
+    }
+
+}
